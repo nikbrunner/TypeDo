a8bee85dd388ffd0a6a820c2c26452affb7050fb
nibru <nikolaus.brunner@protonmail.ch>
2019-09-30 18:01:37 +0200

    Seperated client side functions into a seperate file. Changed some :focus stylings for the inputs

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,16 @@
+import { createTask, clearOutput } from './functions.js';
+
 // DOM Elements
-const UI_inputCommand = document.querySelector("#UI_inputCommand");
-const UI_btnTodoSubmit = document.querySelector("#UI_btnTodoSubmit");
-const UI_output = document.querySelector("#UI_output");
+const UI_inputCommand = document.querySelector('#UI_inputCommand');
+const UI_btnTodoSubmit = document.querySelector('#UI_btnTodoSubmit');
+const UI_output = document.querySelector('#UI_output');
 
-const clearOutput = () => {
-  UI_output.innerHTML = "";
-};
-
-const createTask = input => {
-  let createTask = new Request("/createTask", {
-    method: "post",
-    headers: {"content-type": "application/json"},
-    body: JSON.stringify({
-      command: input.value
-    })
-  });
-
-  fetch(createTask)
-    .then(res => res.json(), err => console.log(err))
-    .then(res => {
-      console.log(res);
-      res = JSON.stringify(res);
-      UI_output.innerHTML = res;
-    });
-};
-
-UI_inputCommand.addEventListener("keydown", e => {
-  if (e.key === "Enter") {
-    createTask(UI_inputCommand);
-  }
+UI_inputCommand.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        createTask(UI_inputCommand);
+    }
 });
 
 UI_btnTodoSubmit.onclick = e => {
-  createTask(UI_inputCommand);
+    createTask(UI_inputCommand);
 };
